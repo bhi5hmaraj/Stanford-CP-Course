ab170867fba54603ffe84975cc7c720f91b92fe8
Bhishmaraj <bs386@snu.edu.in>
2017-04-16 11:08:42 +0530

    Create poj_1001 ( Exponentiation )

---
+++ Coding Excercise/poj_1001.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+import java.math.BigDecimal;
+public class poj_1001 {
+    
+    
+    
+    /************************ SOLUTION STARTS HERE ************************/
+    
+    
+    private static void solve() {
+        
+        String line;
+        while((line = nextLine()) != null) {
+            double a = Double.parseDouble(line.substring(0, 6).trim());
+            int b = Integer.parseInt(line.substring(7).trim());
+            BigDecimal power = BigDecimal.valueOf(a).pow(b);
+            try {
+                println(power.toBigIntegerExact());
+            }
+            catch(ArithmeticException e) {
+                String str = power.toPlainString();
+                if(str.charAt(0) == '0')
+                    str = str.substring(1);
+                println(str);
+            }
+        }
+        
+    }
+    
+    
+    /************************ SOLUTION ENDS HERE ************************/
+    
+    
+    
+    
+    
+    /************************ TEMPLATE STARTS HERE **********************/
+    
+    public static void main(String[] args) throws IOException {
+        reader = new BufferedReader(new InputStreamReader(System.in));
+        writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)), false);
+        st     = null;
+        solve();
+        reader.close();
+        writer.close();
+    }
+    
+    static BufferedReader reader;
+    static PrintWriter    writer;
+    static StringTokenizer st;
+    
+    static String next()
+    {while(st == null || !st.hasMoreTokens()){try{String line = reader.readLine();if(line == null){return null;}            
+    st = new StringTokenizer(line);}catch (Exception e){throw new RuntimeException();}}return st.nextToken();}
+    static String nextLine()  {String s=null;try{s=reader.readLine();}catch(IOException e){e.printStackTrace();}return s;}             
+    static int    nextInt()   {return Integer.parseInt(next());}
+    static long   nextLong()  {return Long.parseLong(next());}     
+    static double nextDouble(){return Double.parseDouble(next());}
+    static char   nextChar()  {return next().charAt(0);}
+    static int[]  nextIntArray(int n)         {int[] a= new int[n];   int i=0;while(i<n){a[i++]=nextInt();}  return a;}
+    static long[] nextLongArray(int n)        {long[]a= new long[n];  int i=0;while(i<n){a[i++]=nextLong();} return a;}    
+    static int[]  nextIntArrayOneBased(int n) {int[] a= new int[n+1]; int i=1;while(i<=n){a[i++]=nextInt();} return a;}            
+    static long[] nextLongArrayOneBased(int n){long[]a= new long[n+1];int i=1;while(i<=n){a[i++]=nextLong();}return a;}            
+    static void   print(Object o)  { writer.print(o);  }
+    static void   println(Object o){ writer.println(o);}
+    
+    /************************ TEMPLATE ENDS HERE ************************/
+    
+}
